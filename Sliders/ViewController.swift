d491137ed2be7f64bba1bcdb900c1cdc23a7b8d6
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2023-08-16 21:47:28 +0300

    Initial Commit

---
+++ Sliders/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Sliders
+//
+//  Created by Илья Десятов on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
